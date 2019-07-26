c9e16bc5625c14b2fdf74020636d3ac4533d944c
Zitor <845811532@qq.com>
2019-07-26 09:28:11 +0800

    来了

---
+++ src/main/java/hh.java
@@ -0,0 +1,11 @@
+/**
+ * @author Zitor
+ * @infos something
+ * @date 2019/7/26
+ */
+public class hh {
+    public static void main(String[] args) {
+
+        System.out.println("22222");
+    }
+}
